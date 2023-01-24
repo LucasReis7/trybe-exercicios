90d85b4a3a7871ad0f30e8992d9a285ef4b29050
Lucas Reis <lucasreis@gmail.com>
2023-01-24 17:02:37 -0300

    finalizando exercicio

---
+++ script3.js
@@ -0,0 +1,9 @@
+const desafioTecnico = 30;
+if (desafioTecnico >= 80){
+    console.log('Parabens vc foi aprovado');
+
+}else if (desafioTecnico < 80 && desafioTecnico >= 60){
+    console.log('Vc está na nossa lista de espera');
+}else{
+    console.log('Infelizmente vc reprovou');
+}

83be378dedc41d9c2f1329da33724df9379c01ab
Lucas Reis <lucasreis@gmail.com>
2023-01-25 11:14:16 -0300

    calculando a media do array

--- script3.js
+++ script3.js
@@ -3,8 +3,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1){
     console.log(numbers[index]);
 }
+
+
 let soma = 0;
 for (let index = 0; index < numbers.length; index += 1){
     soma += numbers[index];
 }
 console.log('A soma do array é: ' + soma); 
+
+
+let media;
+for (let index = 0; index < numbers.length; index += 1){
+    soma += numbers[index];
+}
+media = soma/numbers.length
+console .log('A media do array é: '+ media);

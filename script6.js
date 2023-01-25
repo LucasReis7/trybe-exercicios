766ce0c217f0589aa0128ef9ee568ee055cc3c81
Lucas Reis <lucasreis@gmail.com>
2023-01-25 09:06:44 -0300

    finalizando calculo de salario

--- script6.js
+++ script6.js
@@ -1,2 +1,33 @@
 const sal = 3000;
-const novSal;
+let salBase;
+let salLiq;
+
+
+if (sal <= 1556.94){
+    salBase = sal - sal * 0.08;
+
+}else if(sal > 1556.94 && sal <= 2594.92){
+    salBase = sal - sal * 0.09;
+
+}else if (sal > 2594.92 && sal <= 5189.92){
+    salBase = sal - sal * 0.11;
+
+}else{
+    salBase = sal - 570.88;
+}
+if (salBase <= 1903.98){
+    salLiq = salBase
+
+}else if (salBase > 1903.98 && salBase < 2826.66){
+    salLiq = salBase - (salBase *0.075 - 142.8);
+
+}else if (salBase > 2826.65 && salBase < 3751.06){
+    salLiq = salBase - (salBase *0.15 - 354.8);
+
+}else if (salBase > 3751.05 && salBase < 4664.69){
+    salLiq = salBase - (salBase *0.225 - 636.13);
+
+}else {
+    salLiq = salBase - (salBase *0.275 - 869.36);
+}
+console.log('O salário líquido a ser recebido é R$: ' + salLiq);

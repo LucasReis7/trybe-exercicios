6c116e61d7aa44d6e478079c8b5dec17078a39fe
Lucas Reis <lucasreis@gmail.com>
2023-02-03 13:44:11 -0300

    pegando a maior e menos palabra do array

--- exercicios/index3.js
+++ exercicios/index3.js
@@ -4,8 +4,17 @@ let biggestWord = array[0];
 let smallest = array[0];
 
 for (let index = 0; index < array.length; index++) {
-    if (array) {
-        
+    if (array[index].length > biggestWord.length) {
+        biggestWord = array[index];
     }
     
 }
+
+for (let index = 0; index < array.length; index++) {
+    if (array[index].length < smallest.length) {
+        smallest = array[index];
+    }
+    
+}
+console.log(biggestWord);
+console.log(smallest);

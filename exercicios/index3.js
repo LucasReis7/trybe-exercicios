1656223dbeef8a0498acc2b179e80469442bba40
Lucas Reis <lucasreis@gmail.com>
2023-02-03 15:19:30 -0300

    finaliznado a pratica

--- exercicios/index3.js
+++ exercicios/index3.js
@@ -18,3 +18,19 @@ for (let index = 0; index < array.length; index++) {
 }
 console.log(biggestWord);
 console.log(smallest);
+
+let biggestNumber = 0;
+
+for (let index = 2; index <= 50; index++) {
+    let isPrime = true;
+    for (let index2 = 2; index2 < index; index2++) {
+        if (index % index2 === 0) {
+            isPrime = false;
+        }
+        
+    }
+    if (isPrime) {
+        biggestNumber = index;
+    }
+}
+console.log(biggestNumber);

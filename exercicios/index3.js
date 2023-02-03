c86a0e47873ed543173ac522026dda5a1d6d245b
Lucas Reis <lucasreis@gmail.com>
2023-02-03 13:39:23 -0300

    adicionanado variaveis

--- exercicios/index3.js
+++ exercicios/index3.js
@@ -1,6 +1,11 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 
+let biggestWord = array[0];
+let smallest = array[0];
+
 for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+    if (array) {
+        
+    }
     
 }

4fff3352f18c32d7549f8b62ab8a94acd10fbe4f
Lucas Reis <lucasreis@gmail.com>
2023-02-25 07:30:56 -0300

    adicionando mudança de cor de funda da pagina

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('testando');
+window.onload = () => {
+    let selectBack = document.querySelector('select');
+    selectBack.addEventListener('change', function() {
+        let selectedBack = selectBack.selectedOptions[0];
+        document.body.style.backgroundColor = selectedBack.value;
+
+    })
+}

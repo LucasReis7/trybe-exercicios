ea278185d29628fc2bb8e5f313a1b5721b941d5c
Lucas Reis <lucasreis@gmail.com>
2023-02-25 07:46:20 -0300

    adicionando mudança de cor de letra

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ window.onload = () => {
     let selectBack = document.querySelector('select');
     selectBack.addEventListener('change', function() {
         let selectedBack = selectBack.selectedOptions[0];
-        document.body.style.backgroundColor = selectedBack.value;
-
+        document.body.style.backgroundColor = selectedBack.value
     })
+
+    let selectFont = document.querySelector('#font-color');
+    selectFont.addEventListener('change', function () {
+        let selectedFont = selectFont.selectedOptions[0];
+        document.body.style.color = selectedFont.value
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
